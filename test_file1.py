f168a9fe1587b6542a2a308316a07ae842f48273
Alexander K. Freed <alex.freed@meghcomputing.com>
2022-04-22 17:03:58 -0700

    test ci/diff-fix

---
+++ test_file1.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2021-2022 Megh Computing, Inc.
+
+# All rights reserved. No warranty, explicit or implied, provided.
+# Unauthorized use, modification, or distribution is strictly prohibited.
+
+import sys
+
+if __name__ == "__main__":
+    sys.exit(1)
---
+++ test_file2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2022 Megh Computing, Inc.
+
+# All rights reserved. No warranty, explicit or implied, provided.
+# Unauthorized use, modification, or distribution is strictly prohibited.
+
+import sys
+
+if __name__ == "__main__":
+    sys.exit(2)
